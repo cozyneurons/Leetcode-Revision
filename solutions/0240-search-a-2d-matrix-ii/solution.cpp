3ffc5b3baab012f48c9bc3bd0751f30e14631039
Shubham Kumar <shubhamamn0709@gmail.com>
2026-01-15 18:58:43 +0000

    Sync LeetCode submission Runtime - 54 ms (54.21%), Memory - 18.6 MB (69.26%)

---
+++ solutions/0240-search-a-2d-matrix-ii/solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) 
+    {
+        int n = matrix.size();
+        int row = 0;
+        int column = matrix[0].size()-1 ;
+        while(row<n && column>=0)
+        {
+            if (matrix[row][column]==target) return true;
+            else if (matrix[row][column]<target) row++;
+            else column--;
+        }
+        return false;
+    }
+};
