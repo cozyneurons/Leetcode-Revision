96926d5a0830d46a82c54ce7ef6a58db0b85bd87
Shubham Kumar <shubhamamn0709@gmail.com>
2026-03-02 09:54:52 +0000

    Sync LeetCode submission Runtime - 3 ms (63.88%), Memory - 13.6 MB (36.71%)

---
+++ solutions/0935-orderly-queue/solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string orderlyQueue(string s, int k) 
+    {
+        if (k>1)
+        {
+            sort(s.begin(),s.end());
+            return s;
+        }
+        string result = s;
+        int n = s.size();
+        for (int i = 0; i<n; i++)
+        {
+            string temp = s.substr(i) + s.substr(0,i);
+            result = min(result,temp);
+        }
+        return result;
+    }
+};
