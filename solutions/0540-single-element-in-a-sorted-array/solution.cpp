b665ff630c670c7c2186ff817f886fc7bc46c14d
Shubham Kumar <shubhamamn0709@gmail.com>
2026-03-05 18:22:30 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 26.2 MB (36.36%)

--- solutions/0540-single-element-in-a-sorted-array/solution.cpp
+++ solutions/0540-single-element-in-a-sorted-array/solution.cpp
@@ -4,9 +4,8 @@ public:
     {
         int n = nums.size();
         if (n==1 || nums[0]!=nums[1]) return nums[0];
-        if (nums[n-1]!=nums[n-2]) return nums[n-1];
-        int low = 1;
-        int high = n-2;
+        int low = 0;
+        int high = n-1;
         while(low<=high)
 
         {   
