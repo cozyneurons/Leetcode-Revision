0707dac0002416c445e2f73ef38110e79d965af4
Shubham Kumar <shubhamamn0709@gmail.com>
2026-03-14 10:37:56 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 17 MB (9.12%)

---
+++ solutions/2102-find-the-middle-index-in-array/solution.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int findMiddleIndex(vector<int>& nums) 
+    {
+        int n = nums.size();
+        vector<int> left(n);
+        vector<int> right(n);
+        int presum = 0;
+        int postsum = 0;
+        for (int i = 1; i<n; i++)
+        {
+            presum += nums[i-1];
+            left[i] = presum;
+        }
+        for (int i = n-1; i>=1; i--)
+        {
+            postsum += nums[i];
+            right[i-1] = postsum;
+        }
+        for (int i = 0; i<n; i++)
+        {
+            if (left[i]==right[i]) return i;
+        }
+        return -1;
+    }
+};
