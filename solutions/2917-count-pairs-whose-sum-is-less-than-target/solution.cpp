2a4a796c7e3264367a23ec2e58d2978ddd92de32
Shubham Kumar <shubhamamn0709@gmail.com>
2026-03-16 15:31:29 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 24.4 MB (87.25%)

---
+++ solutions/2917-count-pairs-whose-sum-is-less-than-target/solution.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int countPairs(vector<int>& nums, int target) 
+    {
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+        int i = 0; 
+        int j = n - 1;
+        int count = 0;
+        while (i<j)
+        {
+            if (nums[i]+nums[j]<target)
+            {
+                count += (j-i);
+                i++;
+            }
+            else j--;
+        }
+        return count;
+    }
+};
