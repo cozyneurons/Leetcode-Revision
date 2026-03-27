7e6fd246776998be47d70861470df5d15f9df7ee
Shubham Kumar <shubhamamn0709@gmail.com>
2026-03-27 17:10:54 +0000

    Sync LeetCode submission Runtime - 5 ms (39.24%), Memory - 32.7 MB (63.44%)

--- solutions/0966-binary-subarrays-with-sum/solution.cpp
+++ solutions/0966-binary-subarrays-with-sum/solution.cpp
@@ -1,20 +1,24 @@
 class Solution {
 public:
-    int numSubarraysWithSum(vector<int>& nums, int goal) 
-    {
-        int n = nums.size();
-        unordered_map<int,int> ump;
-        int presum = 0;
-        int count = 0;
-        ump[0] = 1;
-        for (int i = 0; i<n; i++)
+    int atMost(vector<int>& nums, int goal) {
+        if (goal < 0) return 0;
+        int l = 0, sum = 0, count = 0;
+        for (int r = 0; r < nums.size(); r++) 
         {
-            presum += nums[i];
-            int remove = presum - goal;
-            count += ump[remove];
-            ump[presum] += 1;
+            sum += nums[r];
+            while (sum > goal) 
+            {
+                sum -= nums[l];
+                l++;
+            }
+
+            count += (r - l + 1);
         }
         return count;
+    }
+    int numSubarraysWithSum(vector<int>& nums, int goal) 
+    {
+        return atMost(nums, goal) - atMost(nums, goal - 1);
     }
 };
 
