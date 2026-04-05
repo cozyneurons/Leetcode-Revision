cd7de620d7f05558aa86c2b09353693d9f1276b6
Shubham Kumar <shubhamamn0709@gmail.com>
2026-04-05 05:49:38 +0000

    Sync LeetCode submission Runtime - 7 ms (9.12%), Memory - 10.4 MB (46.33%)

---
+++ solutions/0657-robot-return-to-origin/solution.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool judgeCircle(string moves) 
+    {
+        int vertical = 0, horizontal = 0;
+        for (char c : moves)
+        {
+            if (c == 'U') vertical++;
+            else if (c == 'D') vertical--;
+            else if (c == 'R') horizontal++;
+            else if (c == 'L') horizontal--;
+        }
+        return (vertical == 0 && horizontal == 0);
+    }
+};
