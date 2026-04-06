771201e741721cf8330fae65985fb9909224fb25
Shubham Kumar <shubhamamn0709@gmail.com>
2026-04-06 05:47:05 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 13.6 MB (17.75%)

---
+++ solutions/2386-min-max-game/solution.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int minMaxGame(vector<int>& nums) 
+    {
+        int n = nums.size();
+        while (n!=1)
+        {
+            vector<int> newNums(n/2);
+            for (int i = 0; i<n/2; i++)
+            {
+                if (i%2==0)
+                {
+                    newNums[i] = min(nums[2*i],nums[2*i+1]);
+                }
+                else
+                {
+                    newNums[i] = max(nums[2 * i], nums[2 * i + 1]);
+                }
+            }
+            n/=2;
+            nums = newNums;
+        }
+        return nums[0];
+    }
+};
