36c9e26a8d00fe44ec9d499f89303952c73a0c6f
Shubham Kumar <shubhamamn0709@gmail.com>
2026-05-21 12:17:54 +0000

    Sync LeetCode submission Runtime - 1 ms (39.49%), Memory - 17.1 MB (73.90%)

--- solutions/0102-binary-tree-level-order-traversal/solution.cpp
+++ solutions/0102-binary-tree-level-order-traversal/solution.cpp
@@ -25,9 +25,10 @@ public:
             {
                 TreeNode* node = q.front();
                 q.pop();
+                level.push_back(node->val);
                 if (node->left!=NULL) q.push(node->left);
                 if (node->right!=NULL) q.push(node->right);
-                level.push_back(node->val);
+                
             }
             ans.push_back(level);
         }
