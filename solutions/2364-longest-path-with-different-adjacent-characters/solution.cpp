ca1073c3af1431a546eed9b3e208ff93a9e42a56
Shubham Kumar <shubhamamn0709@gmail.com>
2026-05-26 19:38:22 +0000

    Sync LeetCode submission Runtime - 2152 ms (5.05%), Memory - 283 MB (7.49%)

---
+++ solutions/2364-longest-path-with-different-adjacent-characters/solution.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    
+    int result;
+    
+    int DFS(unordered_map<int, vector<int>> &adj, int curr, int parent, string& s) 
+    {
+        
+        int longest = 0;
+        int second_longest = 0;
+        
+        for(int &child : adj[curr]) 
+        {
+            if(child == parent) continue;
+            
+            int child_longest_length = DFS(adj, child, curr, s);
+            
+            if(s[child] == s[curr]) continue;  
+            if(child_longest_length > second_longest) second_longest = child_longest_length;   
+            if(second_longest > longest) swap(longest, second_longest);
+        }
+        
+        int koi_ek_acha = max(longest, second_longest) + 1;
+        int only_root_acha = 1;
+        int neeche_hi_milgaya_answer = longest + second_longest + 1;
+        
+        result = max({result, neeche_hi_milgaya_answer, koi_ek_acha, only_root_acha});
+        
+        return max(koi_ek_acha, only_root_acha);
+        
+    }
+    
+    int longestPath(vector<int>& parent, string s) {
+        int n = parent.size();
+        result = 0;
+        unordered_map<int, vector<int>> adj;
+        for(int i = 1; i<n; i++) 
+        {
+            adj[i].push_back(parent[i]);
+            adj[parent[i]].push_back(i);
+        }
+        DFS(adj, 0, -1, s);
+        return result;
+    }
+};
