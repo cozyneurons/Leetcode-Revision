ed8ae68d5c817c5f7fc643e3efcb3f6ec08cad76
Shubham Kumar <shubhamamn0709@gmail.com>
2026-07-02 18:01:42 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 11.7 MB (48.98%)

--- solutions/0075-sort-colors/solution.cpp
+++ solutions/0075-sort-colors/solution.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     void sortColors(vector<int>& nums) {
         int low = 0, mid = 0, high = nums.size() - 1;
-
+        // DNF Dutch National Flag Algorithm
         while (mid <= high) {
             if (nums[mid] == 0) {
                 swap(nums[low++], nums[mid++]);
