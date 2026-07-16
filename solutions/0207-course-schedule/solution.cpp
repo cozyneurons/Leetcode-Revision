0f7a804cd73505be16c8c439f9699e5f485c67da
Shubham Kumar <shubhamamn0709@gmail.com>
2026-07-16 15:21:25 +0000

    Sync LeetCode submission Runtime - 4 ms (63.79%), Memory - 19.3 MB (55.22%)

--- solutions/0207-course-schedule/solution.cpp
+++ solutions/0207-course-schedule/solution.cpp
@@ -1,34 +1,46 @@
 class Solution {
 public:
-    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-
-        vector<vector<int>> adj(numCourses);
-        vector<int> indegree(numCourses, 0);
-        
-        for (auto &p : prerequisites) {
-            adj[p[1]].push_back(p[0]);
-            indegree[p[0]]++;
-        }
-
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) 
+    {
+        // adj
+        vector<vector<int>> adj (numCourses);
+        // queue
         queue<int> q;
-        for (int i = 0; i < numCourses; i++) {
-            if (indegree[i] == 0)
-                q.push(i);
+        // indeg
+        vector<int> indeg(numCourses,0);
+        // topo 
+        vector<int> topo;
+        for (auto& it: prerequisites)
+        {
+            indeg[it[0]]++;
+            adj[it[1]].push_back(it[0]);
         }
-        int completed = 0;
-
-        while (!q.empty()) {
-            int node = q.front();
-            q.pop();
-            completed++;
-
-            for (auto &nbr : adj[node]) {
-                indegree[nbr]--;
-                if (indegree[nbr] == 0)
-                    q.push(nbr);
+        for (int i = 0; i<numCourses; i++)
+        {
+            if (indeg[i]==0)
+            {
+                q.push(i);
             }
         }
-
-        return completed == numCourses;
+        while (!q.empty())
+        {
+            int node = q.front();
+            q.pop();
+            topo.push_back(node);
+            for (auto& neigh : adj[node])
+            {
+                if (indeg[neigh]>0)
+                {
+                    indeg[neigh]--;
+                    if (indeg[neigh]==0) q.push(neigh);
+                }
+            }
+        }
+        return topo.size()==numCourses;
     }
 };
+
+// topo vector - saare nodes ka relation ki kon kiske pehle aana chayiye
+// {2,4,6,1}.size ! = topo.size() return false
+
+// kahns algo - bfs 
