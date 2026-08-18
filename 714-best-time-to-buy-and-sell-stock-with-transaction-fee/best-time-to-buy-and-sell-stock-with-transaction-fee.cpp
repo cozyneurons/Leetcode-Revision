99678bfeaf097daa17a5e8eb4073d01e5667c283
Shubham Kumar <shubhamamn0709@gmail.com>
2026-08-18 21:56:15 +0530

    Time: 12 ms (75.89%) | Memory: 63.6 MB (81.38%) - LeetSync

--- 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -3,14 +3,16 @@ public:
     int maxProfit(vector<int>& prices, int fee) 
     {
         int n = prices.size();
-        vector<vector<int>> dp(n+1,vector<int>(2,0));
+        vector<int> after(2,0);
+        vector<int> curr(2,0);
         for (int i = n-1; i>=0; i--)
         {
             // holding satate
-            dp[i][0] = max (prices[i] + dp[i+1][1]-fee,dp[i+1][0]);
+            curr[0] = max (prices[i] + after[1]-fee,after[0]);
             // not holding 
-            dp[i][1] = max(-prices[i] + dp[i+1][0],dp[i+1][1]);
+            curr[1] = max(-prices[i] + after[0],after[1]);
+            after = curr;
         }
-        return dp[0][1];
+        return after[1];
     }
 };
