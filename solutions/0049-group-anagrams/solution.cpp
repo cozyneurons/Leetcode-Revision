7862c091e02b7fe2299c616342de8ea50442ca1a
Shubham Kumar <shubhamamn0709@gmail.com>
2026-08-19 06:12:57 +0000

    Sync LeetCode submission Runtime - 18 ms (49.27%), Memory - 26.2 MB (30.57%)

--- solutions/0049-group-anagrams/solution.cpp
+++ solutions/0049-group-anagrams/solution.cpp
@@ -1,29 +1,26 @@
 class Solution {
 public:
-    vector<vector<string>> groupAnagrams(vector<string>& strs) 
-    {
-        int index = 0;
-        unordered_map<string,int> ump;
-        vector<vector<string>> ans;
-        int n = strs.size();
-
-        for (int i = 0; i < n; i++)
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string,vector<string>> mpp;
+        for(auto it:strs)
         {
-            string curr = strs[i];
-            sort(curr.begin(), curr.end());
-
-            if (ump.find(curr) != ump.end())
-            {
-                ans[ump[curr]].push_back(strs[i]);
-            }
-            else
-            {
-                ump[curr] = index;
-                ans.push_back({});
-                ans[index].push_back(strs[i]);
-                index++;
-            }
+            string word=it;
+            sort(it.begin(),it.end());
+            mpp[it].push_back(word);
+        }
+        vector<vector<string>> ans;
+        for(auto it:mpp)
+        {
+            ans.push_back(it.second);
         }
         return ans;
     }
 };
+
+// aet = 0
+// ant = 1
+// abt = 2
+// index = 2
+//{{eat,tea,ate},{tan,nat},{bat}}
+// 3 things
+
