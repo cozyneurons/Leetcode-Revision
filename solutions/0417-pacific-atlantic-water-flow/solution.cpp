9c1bb4f85fbada2b5b897fcea240df3a543870a5
Shubham Kumar <shubhamamn0709@gmail.com>
2026-08-24 19:09:27 +0000

    Sync LeetCode submission Runtime - 18 ms (17.90%), Memory - 22.2 MB (76.44%)

--- solutions/0417-pacific-atlantic-water-flow/solution.cpp
+++ solutions/0417-pacific-atlantic-water-flow/solution.cpp
@@ -1,84 +1,54 @@
+#include <vector>
+using namespace std;
 class Solution {
+private:
+    int m, n;
+    int dr[4] = {-1, 1, 0, 0};
+    int dc[4] = {0, 0, -1, 1};
+
+    // iss reachable 2d vector mei pacific bhi aata hai, atlantic bhi aata hai 
+    void dfs(int r, int c, vector<vector<bool>>& reachable, const vector<vector<int>>& heights) 
+    {
+        reachable[r][c] = true;
+        for (int i = 0; i < 4; i++) 
+        {
+            int nr = r + dr[i];
+            int nc = c + dc[i];
+            if (nr >= 0 && nr < m && nc >= 0 && nc < n && 
+                !reachable[nr][nc] && heights[nr][nc] >= heights[r][c]) 
+            {
+                dfs(nr, nc, reachable, heights);
+            }
+        }
+    }
 public:
     vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
-        int m = heights.size(), n = heights[0].size();
+        if (heights.empty() || heights[0].empty()) return {};
+        m = heights.size();
+        n = heights[0].size();
         vector<vector<bool>> pacific(m, vector<bool>(n, false));
         vector<vector<bool>> atlantic(m, vector<bool>(n, false));
-        for (int i = 0; i < m; i++) 
-        {
-            pacific[i][0] = true;
-            atlantic[i][n-1] = true;
-        }
         for (int j = 0; j < n; j++) 
         {
-            pacific[0][j] = true;
-            atlantic[m-1][j] = true;
-        }   
-        bool updated = true;
-        while(updated) 
+            dfs(0, j, pacific, heights);
+            dfs(m - 1, j, atlantic, heights);
+        }
+        for (int i = 0; i < m; i++) 
         {
-            updated = false;
-            for (int i = 0; i < m; i++) 
-            {
-                for (int j = 0; j < n; j++) 
-                {
-                    if (!pacific[i][j]) 
-                    {
-                        if (i > 0 && pacific[i-1][j] && heights[i][j] >= heights[i-1][j]) 
-                        {
-                            pacific[i][j] = true;
-                            updated = true;
-                        } 
-                        else if (i < m-1 && pacific[i+1][j] && heights[i][j] >= heights[i+1][j]) 
-                        {
-                            pacific[i][j] = true;
-                            updated = true;
-                        } 
-                        else if (j > 0 && pacific[i][j-1] && heights[i][j] >= heights[i][j-1]) 
-                        {
-                            pacific[i][j] = true;
-                            updated = true;
-                        } 
-                        else if (j < n-1 && pacific[i][j+1] && heights[i][j] >= heights[i][j+1]) 
-                        {
-                            pacific[i][j] = true;
-                            updated = true;
-                        }
-                    } 
-                    if (!atlantic[i][j]) 
-                    {
-                        if (i > 0 && atlantic[i-1][j] && heights[i][j] >= heights[i-1][j]) 
-                        {
-                            atlantic[i][j] = true;
-                            updated = true;
-                        } 
-                        else if (i < m-1 && atlantic[i+1][j] && heights[i][j] >= heights[i+1][j]) 
-                        {
-                            atlantic[i][j] = true;
-                            updated = true;
-                        } 
-                        else if (j > 0 && atlantic[i][j-1] && heights[i][j] >= heights[i][j-1]) 
-                        {
-                            atlantic[i][j] = true;
-                            updated = true;
-                        } 
-                        else if (j < n-1 && atlantic[i][j+1] && heights[i][j] >= heights[i][j+1]) 
-                        {
-                            atlantic[i][j] = true;
-                            updated = true;
-                        }
-                    }
-                }
-            }
+            dfs(i, 0, pacific, heights);
+            dfs(i, n - 1, atlantic, heights);
         }
         vector<vector<int>> result;
         for (int i = 0; i < m; i++) 
         {
             for (int j = 0; j < n; j++) 
             {
-                if (pacific[i][j] && atlantic[i][j]) result.push_back({i, j});
+                if (pacific[i][j] && atlantic[i][j]) 
+                {
+                    result.push_back({i, j});
+                }
             }
-        } 
+        }
         return result;
     }
 };
