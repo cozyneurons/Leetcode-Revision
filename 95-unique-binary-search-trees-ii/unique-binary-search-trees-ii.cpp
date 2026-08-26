f9cfd70bc3da5f7c0646e5797f9fa8c25aa96c92
Shubham Kumar <shubhamamn0709@gmail.com>
2026-08-27 00:07:39 +0530

    Time: 0 ms (100.00%) | Memory: 16.8 MB (96.17%) - LeetSync

--- 95-unique-binary-search-trees-ii/unique-binary-search-trees-ii.cpp
+++ 95-unique-binary-search-trees-ii/unique-binary-search-trees-ii.cpp
@@ -1,12 +1,25 @@
 class Solution {
 private:
+    map<pair<int,int>, vector<TreeNode*>> mp;
     vector<TreeNode*> solve(int start, int end) {
         if (start > end) return {nullptr};
         vector<TreeNode*> result;
         for (int i = start; i <= end; i++) 
         {
-            vector<TreeNode*> left_bsts = solve(start, i - 1);
-            vector<TreeNode*> right_bsts = solve(i + 1, end);
+            vector<TreeNode*> left_bsts;
+            vector<TreeNode*> right_bsts;
+            if (mp.find({start,i-1})==mp.end())
+            {
+                left_bsts = solve(start, i - 1);
+                mp[{start,i-1}] = left_bsts;
+            } 
+            else left_bsts = mp[{start,i-1}];
+            if (mp.find({i+1,end})==mp.end())
+            {
+                right_bsts = solve(i + 1, end);
+                mp[{i+1,end}] = right_bsts;
+            } 
+            else right_bsts = mp[{i+1,end}];
             for (TreeNode* leftNode : left_bsts) 
             {
                 for (TreeNode* rightNode : right_bsts) 
