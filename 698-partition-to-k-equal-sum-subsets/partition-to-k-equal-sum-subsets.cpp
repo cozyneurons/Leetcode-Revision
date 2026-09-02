fd23e0d5c8bf7a9a88262e74c735276486728cc7
Shubham Kumar <shubhamamn0709@gmail.com>
2026-09-02 23:29:22 +0530

    Time: 0 ms (100.00%) | Memory: 12.2 MB (51.56%) - LeetSync

--- 698-partition-to-k-equal-sum-subsets/partition-to-k-equal-sum-subsets.cpp
+++ 698-partition-to-k-equal-sum-subsets/partition-to-k-equal-sum-subsets.cpp
@@ -1,40 +1,34 @@
 class Solution {
-private:
-    int sum;
-    bool recursion(int ind, vector<int>& nums,vector<int>& track,int k,int ori_k,int req_sum)
-    {
-        // base case
-        if (k==1) return true;
-        int n = nums.size();
-        // ek subset ban gaya -> target -> k-1 subsets
-        if (req_sum==0)
-        {
-            if (recursion(0,nums,track,k-1,ori_k,sum/ori_k)) return true;
-        }
-        for (int i = ind; i<n; i++)
-        {
-            // already picked
-            if (track[i]==1) continue;
-            if (nums[i]>req_sum) continue;
-            // pick
-            track[i] = 1;
-            if (recursion(i+1,nums,track,k,ori_k,req_sum-nums[i])) return true;
-            track[i] = 0;
-            if (req_sum==0) return false;
-        }
-        return false;
+public:
+    bool backtracking(int k ,int i,vector<int>& subset,vector<int>&sides,int length){
+    int n = subset.size();    
+    if(i==n){
+        return true;
     }
 
-public:
-    bool canPartitionKSubsets(vector<int>& nums, int k) 
-    {
-        int n = nums.size();
-        sum = accumulate(nums.begin(),nums.end(),0);
-        // recursion
-        // vector track 
-        vector<int> track(n,0);
-        if (sum%k!=0) return false;
-        sort(nums.rbegin(),nums.rend());
-        return recursion(0,nums,track,k,k,sum/k);
+    for(int j = 0 ; j < k ; j++){
+        if(sides[j] + subset[i] <= length){
+           sides[j]+=subset[i];
+           if(backtracking(k,i+1,subset,sides,length)){
+            return true;
+           }
+           sides[j]-=subset[i];
+           if(sides[j]==0)break;
+        }
     }
+    return false;
+
+    }
+    bool canPartitionKSubsets(vector<int>& nums, int k) {
+        int sum = accumulate(nums.begin(),nums.end(),0);
+        float length = sum/k;
+        vector<int>sides(k,0);
+
+        if(sum/k!=length) return false;
+
+        sort(nums.rbegin(),nums.rend());
+
+        return backtracking(k,0,nums,sides,length);
+    }
+    
 };
