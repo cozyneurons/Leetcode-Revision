1908e6351c36f2638d27a608e8ff07b5d7d00a42
Shubham Kumar <shubhamamn0709@gmail.com>
2026-09-11 22:52:55 +0530

    Time: 2 ms (94.02%) | Memory: 10.5 MB (93.49%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -12,7 +12,8 @@ public:
         for (int i = 1; i<=m; i++) 
         {
             curr[0] = i;
-            for (int j = 1; j <= n; j++) {
+            for (int j = 1; j <= n; j++) 
+            {
                 if (word1[i - 1] == word2[j - 1]) 
                 {
                     curr[j] = prev[j - 1];
