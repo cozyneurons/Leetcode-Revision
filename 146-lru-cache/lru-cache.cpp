f05114265a314d3ea373ccb72a56fae833ea37e0
Shubham Kumar <shubhamamn0709@gmail.com>
2026-09-11 17:01:53 +0530

    Time: 15 ms (86.29%) | Memory: 48.2 MB (78.22%) - LeetSync

--- 146-lru-cache/lru-cache.cpp
+++ 146-lru-cache/lru-cache.cpp
@@ -1,101 +1,116 @@
 class LRUCache {
-public:
+private:
+    int current_size;
     int n;
-    int curr;
-    struct Node 
-    {
+    
+public:
+    struct Node{
         int key;
         int val;
-        Node* next;
         Node* prev;
-        
-        // Constructor to easily create a new node
-        Node(int key, int val) {
-            this->key = key;
-            this->val = val;
-            next = NULL;
+        Node* next;
+        Node(int k, int v) {
+            key = k;
+            val = v;
             prev = NULL;
+            next = NULL;
         }
     };
+    
+    unordered_map<int, Node*> ump;
     Node* head;
     Node* tail;
-    // Map stores the key, and a pointer (iterator) to the node in the list.
-    unordered_map<int, Node*> ump;
-    LRUCache(int capacity) 
-    {
+    
+    LRUCache(int capacity) {
+        current_size = 0;
         n = capacity;
-        curr = 0;
         head = new Node(-1, -1);
         tail = new Node(-1, -1);
+
         head->next = tail;
+        tail->next = NULL;
+
         tail->prev = head;
+        head->prev = NULL;
     }
     
-    int get(int key) 
-    {
-        if (ump.find(key)==ump.end())
-        {
+    // Optional but good practice: Destructor to clean up memory
+    ~LRUCache() {
+        Node* curr = head;
+        while (curr != NULL) {
+            Node* nextNode = curr->next;
+            delete curr;
+            curr = nextNode;
+        }
+    }
+
+    void pbh(Node* head, Node* tail, Node* curr) {
+        Node* front = head->next;
+        head->next = curr;
+        curr->next = front;
+        front->prev = curr;
+        curr->prev = head;
+    }
+    
+    void dfb(Node* head, Node* tail) {
+        Node* lru = tail->prev;
+        Node* new_lru = lru->prev;
+        new_lru->next = tail;
+        tail->prev = new_lru;
+        delete(lru);
+    }
+    
+    void rfp(Node* head, Node* tail, Node* curr) {
+        Node* left = curr->prev;
+        Node* right = curr->next;
+        left->next = right;
+        right->prev = left;
+    }
+    
+    int get(int key) {
+        if (ump.find(key) != ump.end()) {
+            Node* curr = ump[key];
+            // remove from its current position
+            rfp(head, tail, curr);    
+            // add to head
+            pbh(head, tail, curr);
+            return ump[key]->val;
+        } else {
             return -1;
         }
-        Node* most_recent = ump[key];
-        Node* piche = most_recent->prev;
-        Node* aage = most_recent->next;
-        piche->next = aage;
-        aage->prev = piche;
-
-        Node* front = head->next;
-        most_recent->next = front;
-        most_recent->prev = head;
-        front->prev = most_recent;
-        head->next = most_recent;
-        return ump[key]->val;
     }
     
-    void put(int key, int value) 
-    {
-        // agar map mei nahi hai toh add karna hai
-        if (ump.find(key)==ump.end())
-        {
-            curr++;
-            if (curr>n)
-            {
-                // agar cache ka size capacity se jyada ho raha hai toh remove least recently used node 
-                Node* least_recent = tail->prev;
-                Node* back = least_recent->prev;
-                back->next = tail;
-                tail->prev = back;
-                ump.erase(least_recent->key);
-                delete(least_recent);
-                curr--;
-            }
-            Node* New = new Node(key,value);
-            Node* front = head->next;
-            New->next = front;
-            New->prev = head;
-            front->prev = New;
-            head->next = New;
-            ump[key] = New;
-        }
-        else
-        {
-            // just update
+    void put(int key, int value) {
+        // Case 1: Key already exists
+        if (ump.find(key) != ump.end()) {
             ump[key]->val = value;
-            // put this key in front because it is recently used
-            Node* most_recent = ump[key];
-            Node* piche = most_recent->prev;
-            Node* aage = most_recent->next;
-            piche->next = aage;
-            aage->prev = piche;
-
-            Node* front = head->next;
-            most_recent->next = front;
-            most_recent->prev = head;
-            front->prev = most_recent;
-            head->next = most_recent;
+            Node* curr = ump[key];
+            rfp(head, tail, curr);
+            pbh(head, tail, curr);
+            return; // We are done, exit early
         }
+        
+        // Case 2: Key doesn't exist, and cache is FULL
+        if (current_size == n) {
+            // Get the LRU key before deleting the node
+            int lru_key = tail->prev->key; 
+            
+            // Delete from map FIRST, using the LRU key
+            ump.erase(lru_key); 
+            
+            // Now delete from the linked list
+            dfb(head, tail); 
+        } else {
+            // Case 3: Key doesn't exist, and we have room
+            current_size++;
+        }
+        
+        // Add the new node to the map and linked list
+        Node* curr = new Node(key, value);
+        ump[key] = curr;
+        pbh(head, tail, curr);
     }
 };
-
 /**
  * Your LRUCache object will be instantiated and called as such:
  * LRUCache* obj = new LRUCache(capacity);
